342e561ee3dd24899fca07468fd094fc2a058481
ValentinaGómez <140546534+Valentina-21@users.noreply.github.com>
2023-10-06 03:54:54 -0500

    cinco

--- src/components/PokedexPage/PokeCard.jsx
+++ src/components/PokedexPage/PokeCard.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import useFetch from "../../hooks/useFetch"
 import { useNavigate } from "react-router-dom"
-import './style/PokeCard.css'
+import './style/Pokecard.css'
 
 
 
